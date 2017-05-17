d58c3d58819ab49cd5d566b2f8f9e571bd103003
bema <bema@DESKTOP-NTQ29PF>
2017-05-17 09:47:26 +0800

    修改pom文件,添加中央仓库

--- product-business-fe/src/main/java/com/zcbspay/platform/business/fe/controller/SignatureController.java
+++ product-business-fe/src/main/java/com/zcbspay/platform/business/fe/controller/SignatureController.java
@@ -1,7 +1,5 @@
 package com.zcbspay.platform.business.fe.controller;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.zcbspay.platform.business.concentrate.batch.service.BatchCollection;
-import com.zcbspay.platform.business.concentrate.batch.service.BatchPayment;
-import com.zcbspay.platform.business.concentrate.bean.RealtimeCollectionBean;
-import com.zcbspay.platform.business.concentrate.bean.RealtimePaymentBean;
-import com.zcbspay.platform.business.concentrate.bean.ResultBean;
-import com.zcbspay.platform.business.concentrate.realtime.service.RealtimeCollection;
-import com.zcbspay.platform.business.concentrate.realtime.service.RealtimePayment;
 import com.zcbspay.platform.support.signaturn.bean.AdditBean;
 import com.zcbspay.platform.support.signaturn.bean.MessageBean;
 import com.zcbspay.platform.support.signaturn.service.MessageDecodeService;
 import com.zcbspay.platform.support.signaturn.service.MessageEncryptService;
 
 import net.sf.json.JSONObject;
-import net.sf.json.util.JSONUtils;
 
 
 @Controller
